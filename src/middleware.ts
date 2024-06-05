72ebe881774fa94e2e49a9892c8d6f4b634fd174
kikemaya <enrique.mg.cd@gmail.com>
2024-06-05 12:04:17 -0600

    Adding cookie configs

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,12 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === "/set-cookie") {
     const response = NextResponse.redirect(new URL("/", request.url));
+    
     response.headers.set(
       "Set-Cookie",
-      "__session=hey it's working; Path=/; Secure; HttpOnly; SameSite=lax"
+      "__session=hey it's working; Path=/; Secure; HttpOnly; SameSite=lax; public; max-age=300; s-maxage=600"
     );
+    
     return response;
   }
   return NextResponse.next();
